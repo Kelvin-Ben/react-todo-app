ee70f5aa3a5e4c4ba0c606b29901e1c9ba2df902
Kelvin Ben <kevoben14@gmail.com>
2022-11-01 22:34:32 +0300

    Working with components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,8 @@
-i
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1>Todos</h1>
+  </header>
+);
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoContainer from './components/TodoContainer';
+
+// const element = <h1>Hello form react App</h1>;
+ReactDOM.render(<React.StrictMode><TodoContainer /></React.StrictMode>, document.getElementById('root'));
